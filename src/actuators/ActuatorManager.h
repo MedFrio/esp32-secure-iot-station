2968435a3d8d47b255935bb811a5b01cf547767f
Mohammed FRIOUICHEN <98142644+MedFrio@users.noreply.github.com>
2026-06-16 16:13:58 +0200

    Add servo sweep functionality to ActuatorManager

--- src/actuators/ActuatorManager.h
+++ src/actuators/ActuatorManager.h
@@ -8,12 +8,18 @@ struct ActuatorState {
   bool ledEnabled = false;
   uint8_t ledBrightness = 180;
   int servoAngle = 90;
+  bool servoSweepEnabled = false;
+  uint8_t servoSweepIntensity = 50;
+  uint8_t servoSweepMode = 0;
+  bool emergencyStopActive = false;
   uint32_t lastCommandMs = 0;
 };
 
 class ActuatorManager {
 public:
   void begin();
+  void update();
+  void setEmergencyStop(bool active);
   bool applyCommand(JsonVariantConst json, String& error);
   ActuatorState state() const;
   void writeStateTo(JsonObject target) const;
@@ -25,9 +31,15 @@ private:
 
   ActuatorState currentState;
   Servo servo;
+  int8_t servoSweepDirection = 1;
+  uint32_t lastServoSweepMs = 0;
+  uint8_t servoPatternIndex = 0;
 
   void applyLed();
   void applyServo();
+  void updateServoSweep();
+  void updateServoRadar(uint8_t intensity);
+  void updateServoAlert(uint8_t intensity);
   bool updateLed(JsonVariantConst led, String& error);
   bool updateServo(JsonVariantConst servoJson, String& error);
 };
